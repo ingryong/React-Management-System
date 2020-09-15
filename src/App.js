5bdd7d831bdfa48dee68d17d2389bba3b20029ce
ingryong <bbgod2@gmail.com>
2020-09-15 23:09:35 +0900

    20.09.15

--- src/App.js
+++ src/App.js
@@ -1,12 +1,49 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Customer from "./components/Customer";
 
+const customers = [
+  {
+    id: 1,
+    image: "https://placeimg.com/64/64/1",
+    name: "홍길동",
+    birthday: "961222",
+    gender: "남자",
+    job: "대학생",
+  },
+  {
+    id: 2,
+    image: "https://placeimg.com/64/64/2",
+    name: "백철우",
+    birthday: "920206",
+    gender: "남자",
+    job: "디자이너",
+  },
+  {
+    id: 3,
+    image: "https://placeimg.com/64/64/3",
+    name: "백세진",
+    birthday: "940109",
+    gender: "여자",
+    job: "바리스타",
+  },
+];
 function App() {
   return (
-    <div className=".ray-background">
-      <img src={logo} lat="logo" />
-      <h2>Let's develop React Management System!</h2>
+    <div>
+      {customers.map((c) => {
+        return (
+          <Customer
+            key={c.id}
+            id={c.id}
+            image={c.image}
+            name={c.name}
+            birthday={c.birthday}
+            gender={c.gender}
+            job={c.job}
+          />
+        );
+      })}
     </div>
   );
 }
